ad51acc589e5899f848b0f12b6fecaed2cd16c5c
Ana María <anamariasosam@gmail.com>
2018-02-02 14:56:43 -0500

    Add authentication with firebase

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import reducers from './reducers'
+import ReduxThunk from 'redux-thunk'
+import firebase from 'firebase'
+
+import LoginForm from './components/LoginForm'
 
 export default class App extends React.Component {
+
+  authentication() {
+    var config = {
+      apiKey: 'AIzaSyCNj4qK9OFRgxpYgvsR-Ee0kxD4WI6aUiI',
+      authDomain: 'team-work-9079d.firebaseapp.com',
+      databaseURL: 'https://team-work-9079d.firebaseio.com',
+      projectId: 'team-work-9079d',
+      storageBucket: 'team-work-9079d.appspot.com',
+      messagingSenderId: '799878237976'
+    };
+    firebase.initializeApp(config);
+  }
+
+  componentWillMount() {
+    this.authentication()
+  }
+
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store} >
+        <LoginForm />
+      </Provider>
+
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
